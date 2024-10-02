c6e944f9c0705a7b220141023faf720f0530b0bf
Rey Mark Solsona <rey.solsona@readytech.io>
2024-10-02 23:01:52 +0800

    Implemented angular-calendar library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../app/pages/root/app.component';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './pages/shared/layout/header/header.component';
 import { SidebarComponent } from './pages/shared/layout/sidebar/sidebar.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/moment';
 import * as moment from 'moment';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarComponent } from './pages/feature/calendar/calendar.component';
+import { FlatpickrModule } from 'angularx-flatpickr';
 
 export function momentAdapterFactory() {
   return adapterFactory(moment);
@@ -19,14 +22,19 @@ export function momentAdapterFactory() {
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
+    NgbModalModule,
     CommonModule,
+    FlatpickrModule.forRoot(),
     ReactiveFormsModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: momentAdapterFactory })
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: momentAdapterFactory }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
