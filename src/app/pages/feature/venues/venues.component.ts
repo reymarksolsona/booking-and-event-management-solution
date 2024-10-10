d713394fcf8d6f8de4d459b8e84b1e724aaabf6d
Rey Mark Solsona <solsonareymark@yahoo.com>
2024-10-10 13:09:48 +0800

    - Added pinning of location on modal

--- src/app/pages/feature/venues/venues.component.ts
+++ src/app/pages/feature/venues/venues.component.ts
@@ -22,33 +22,77 @@ export class VenuesComponent implements OnInit {
     this.activeTab = tabName
   }
   
-  initializeMap(mapId: string = 'main-map'): void {
+  initializeMap(mapId: string = 'main-map', selectedEvent: any = null): void {
+    let center = [];
+    if (mapId === 'main-map') {
+      center = [151.949997, -27.566668];
+    } else {
+      center = [selectedEvent.coordinates[1], selectedEvent.coordinates[0]];
+    }
     const map = new Map(mapId, {
-      center: [151.949997, -27.566668],
+      center: center,
       zoom: 8,
       authOptions: {
         authType: AuthenticationType.subscriptionKey,
         subscriptionKey: 'Hk9mGeCHSYkZzNMwWkizYHrbu3DZywS7r7yWafAt303oSuUHjXySJQQJ99AJAC5RqLJwadhfAAAgAZMPoJep'
       }
     });
-
-    this.eventsCalendarEvents.forEach(event => {
-      const coordinates = event.coordinates; // Assumed you have a `coordinates` property with [lat, lon]
-  
-      if (coordinates && coordinates.length === 2) {
-        const lngLat = [coordinates[1], coordinates[0]]; // Reverse to [lon, lat]
+    if (mapId === 'main-map') {
+      this.eventsCalendarEvents.forEach(event => {
+        const coordinates = event.coordinates; // Assumed you have a `coordinates` property with [lat, lon]
     
-        // Create a marker for the event
-        const marker = new atlas.HtmlMarker({
-          position: lngLat, // Use the correctly ordered lngLat
-          text: event.title,  // Display event title on marker
-          color: '#dc3545' // You can customize the marker color
-        });
-    
-        // Add the marker to the map
-        map.markers.add(marker);
-      }
-    });
+        if (coordinates && coordinates.length === 2) {
+          const lngLat = [coordinates[1], coordinates[0]]; // Reverse to [lon, lat]
+      
+          // Create a marker for the event
+          const marker = new atlas.HtmlMarker({
+            position: lngLat, // Use the correctly ordered lngLat
+            text: event.title,  // Display event title on marker
+            color: '#dc3545'
+          });
+          const popup = new atlas.Popup({
+            content: `<div><strong>${event.title}</strong><br>Contact No.: ${event.contact}</div>`, // Customize popup content
+            position: [lngLat[0], lngLat[1] + 0.050]
+          });
+          // Add the marker to the map
+          map.markers.add(marker);
+          marker.getElement().addEventListener('click', () => {
+            // Open or close the popup on click
+            if (popup.isOpen()) {  // Call isOpen() method
+              popup.close();
+            } else {
+              popup.open(map);
+            }
+          });
+        }
+      });
+    } else {
+      const coordinates = selectedEvent?.coordinates; // Assumed you have a `coordinates` property with [lat, lon]
+    console
+        if (coordinates && coordinates.length === 2) {
+          const lngLat = [coordinates[1], coordinates[0]]; // Reverse to [lon, lat]
+      
+          // Create a marker for the event
+          const marker = new atlas.HtmlMarker({
+            position: lngLat, // Use the correctly ordered lngLat
+            color: '#dc3545'
+          });
+          const popup = new atlas.Popup({
+            content: `<div><strong>${selectedEvent?.title}</strong><br>Contact No.: ${selectedEvent?.contact}</div>`, // Customize popup content
+            position: [lngLat[0], lngLat[1] + 0.050]
+          });
+          // Add the marker to the map
+          map.markers.add(marker);
+          marker.getElement().addEventListener('click', () => {
+            // Open or close the popup on click
+            if (popup.isOpen()) {  // Call isOpen() method
+              popup.close();
+            } else {
+              popup.open(map);
+            }
+          });
+        }
+    }
   }
 
   actions: CalendarEventAction[] = [
@@ -65,69 +109,6 @@ export class VenuesComponent implements OnInit {
       },
     },
   ];
-
-
-  // stashedeventsCalendarEvents: TRCBookableSpace[] = [
-  //   {
-  //     title: 'Cecil Plains Library',
-  //     start: startOfDay(new Date('2024-10-20')),
-  //     end: startOfDay(new Date('2024-10-21')),
-  //     color: { primary: '#ad2121', secondary: '#FAE3E3' },
-  //     actions: this.actions,
-  //     allDay: true,
-  //     meta: {
-  //       description: 'Get ready to embrace the joy of longer days and vibrant styles! Our Spring Fashion Weekend is here!',
-  //       recurring: true
-  //     },
-  //     imgUrl: 'https://www.tr.qld.gov.au/images/500-facilityRecreation/510-libraries/511-libraryLocationHours/cecil_plains.jpg',
-  //     location: 'Taylor Street, Cecil Plains',
-  //     contact: '4668 0250',
-  //     openHours: [
-  //       "Wednesday - 10am to 1pm & 2pm to 5pm",
-  //       "Friday - 10am to 1pm & 2pm to 5pm"
-  //     ]
-  //   },
-  //   {
-  //     title: 'Clifton Library',
-  //     start: startOfDay(new Date('2024-10-20')),
-  //     end: startOfDay(new Date('2024-10-21')),
-  //     color: { primary: '#ad2121', secondary: '#FAE3E3' },
-  //     actions: this.actions,
-  //     allDay: true,
-  //     meta: {
-  //       description: 'Get ready to embrace the joy of longer days and vibrant styles! Our Spring Fashion Weekend is here!',
-  //       recurring: true
-  //     },
-  //     imgUrl: 'https://www.tr.qld.gov.au/images/500-facilityRecreation/510-libraries/511-libraryLocationHours/clifton.jpg',
-  //     location: '8 Meara Place, Clifton',
-  //     contact: '4697 4223',
-  //     openHours: [
-  //       "Tuesday - 9.30am to 1pm & 2pm to 5pm",
-  //       "Wednesday - 9.30am to 1pm & 2pm to 5pm",
-  //       "Thursday - 9.30am to 1pm & 2pm to 5pm"
-  //     ]
-  //   },
-  //   {
-  //     title: 'Highfields Cultural Centre',
-  //     start: startOfDay(new Date('2024-10-20')),
-  //     end: startOfDay(new Date('2024-10-21')),
-  //     color: { primary: '#ad2121', secondary: '#FAE3E3' },
-  //     actions: this.actions,
-  //     allDay: true,
-  //     meta: {
-  //       description: 'Get ready to embrace the joy of longer days and vibrant styles! Our Spring Fashion Weekend is here!',
-  //       recurring: true
-  //     },
-  //     imgUrl: 'https://www.tr.qld.gov.au/images/500-facilityRecreation/550-functionCentres/Highfields%20CulturalCentrearticlewide.jpg#joomlaImage://local-images/500-facilityRecreation/550-functionCentres/Highfields%20CulturalCentrearticlewide.jpg?width=870&height=423',
-  //     location: "O'Brien Road, Highfields",
-  //     contact: ' 07 4699 6500',
-  //     openHours: [
-  //       "Tuesday - 9.30am to 1pm & 2pm to 5pm",
-  //       "Wednesday - 9.30am to 1pm & 2pm to 5pm",
-  //       "Thursday - 9.30am to 1pm & 2pm to 5pm"
-  //     ]
-  //   }
-  // ];
 
   eventsCalendarEvents: TRCBookableSpace[] = [
     {
@@ -195,14 +176,6 @@ export class VenuesComponent implements OnInit {
   ];selectedEvent: TRCBookableSpace | null = null;
   constructor(private modalService: NgbModal,private msalService: MsalService) {}
   ngOnInit() {
-    // this.msalService.instance.acquireTokenSilent({
-    //   scopes: ['YOUR_AZURE_MAPS_SCOPE']
-    // }).then((response:any) => {
-    //   this.initializeMap(response.accessToken);
-    // }).catch((error:any) => {
-    //   console.error('Token acquisition failed:', error);
-    //   // Fallback: Optionally, handle token acquisition failure
-    // });
      this.initializeMap();
     this.loadEventsFromLocalStorage();
   }
@@ -215,7 +188,7 @@ export class VenuesComponent implements OnInit {
   openMap(trcEvent: TRCBookableSpace) {
     this.selectedEvent = trcEvent;
     this.modalService.open(this.mapView, { size: 'lg' });
-    setTimeout(() => this.initializeMap('modal-map'), 2000);
+    setTimeout(() => this.initializeMap('modal-map', this.selectedEvent), 2000);
   }
   loadEventsFromLocalStorage() {
     const lsEvents = localStorage.getItem('lsEvents');
